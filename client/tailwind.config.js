e74c08f629108c3ff8b41f1a40bc5c36e1d24381
Michael Zhao <michaelzhao314@gmail.com>
2024-10-06 09:22:28 -0500

    Finished onboarding pages

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "poppins": '"Poppins", Inter, system-ui, Avenir, Helvetica, Arial, sans-serif'
+      }
+    },
   },
   plugins: [],
 }
